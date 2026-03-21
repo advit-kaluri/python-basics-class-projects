b5808edecf4543194ad8a55c28fa53fecaacd3b7
advit-kaluri <arjunkaluri@gmail.com>
2026-03-21 12:42:30 +0530

    Add files via upload

---
+++ Getting start with programming/print.py
@@ -0,0 +1,20 @@
+print("ji my name is advit")
+print(23)
+print(3+4)
+a=5
+print(a)
+print("my name is advit",end='.')
+
+      
+
+
+
+
+      
+      
+       
+
+
+
+
+
---
+++ Getting start with programming/varirable.py
@@ -0,0 +1,5 @@
+name=("advit")
+Name=("advit")
+_name=("advit")
+frist_name=("advit")
+
